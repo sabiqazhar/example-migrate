7fde3ab9fb73809b480ddcaffc4d012421ad7dfb
sabiqaz <sabiqandazhar@gmail.com>
2023-12-13 11:10:49 +0700

    add member_number at accounts

---
+++ database/migrations/2023_12_12_092318_add_member_number.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('accounts', function($table){
+            $table->string('member_number')->nullable()->after('password');
+            $table->string('point')->nullable()->after('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('accounts', function($table){
+            $table->dropColumn('member_number');
+            $table->dropColumn('point');
+        });
+    }
+};
